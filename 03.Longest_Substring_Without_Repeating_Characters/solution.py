7b8c9531ec465f32e605c87422dffa453f8deb96
Takuma Doi <d01tkum@Takumas-MacBook-Pro.local>
2024-01-23 20:01:37 +0900

    2024.01.23 2th

--- 03.Longest_Substring_Without_Repeating_Characters/solution.py
+++ 03.Longest_Substring_Without_Repeating_Characters/solution.py
@@ -1,18 +1,22 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         '''
-        ・substring の長さが s の長さの半分を超えたら、その時点でその substring が最大
+        # Sliding Window
+        1. index 0 から既出の文字が出るまで走査する
+        2. index を 1 つ進める度に、ans を更新 max(ans, j - i + 1)
+        3. もし既出の文字が無ければ、ans が答え
+        4. もし既出の文字が有れば、
+            1. それまでの箇所は確認済みなので、既出文字までを腐り落とし、既出文字の次を新たな始点とする
         '''
         length = len(s)
-        
-        maximum = 0
-        for start in range(length):     #O(n)
-            for end in range(start, length):   #O(n)
-                sub_s = s[start:end+1]
+        hashmap = dict()
+        ans = 0
 
-                if len(sub_s) != len(set(sub_s)):
-                    break
-            if end - start + 1 > maximum:
-                maximum = end - start + 1
-        return maximum
-
+        i = 0
+        for j in range(length):
+            if s[j] in hashmap.keys():
+                i = max(hashmap[s[j]], i)   # 現在の始点よりも前に登場している場合は影響しない為スキップ
+                
+            ans = max(ans, j - i + 1)
+            hashmap[s[j]] = j + 1
+        return ans
